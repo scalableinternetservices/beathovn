6c54ad496a1985f51fcc3fa22de21a888f1ae33b
Kyle Tran <kylevtran201@gmail.com>
2020-11-13 15:52:51 -0800

    Added like button, actual song link, and fixed border padding margins

--- web/src/view/page/Post.tsx
+++ web/src/view/page/Post.tsx
@@ -9,16 +9,26 @@ const cardStyle: CSS.Properties = {
   boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)'
 }
 
+const buttonStyle: CSS.Properties = {
+  backgroundColor: '#000000',
+  color: '#FFFFFF'
+}
+
+function likeButtonHandler() {
+  console.log("The like button was clicked")
+}
+
 export function Post(props: {
   commentary: string | null
-  musicLink: string | null
+  musicLink: string | undefined
 }) {
 
   return (
     <div className="card" style={cardStyle}>
       <div className="container" style={containerStyle}>
-        <p>{props.musicLink}</p>
+        <a href={props.musicLink} target="_blank">Song Link</a>
         <h3>{props.commentary}</h3>
+        <button style={buttonStyle} type="button" onClick={likeButtonHandler}>Like</button>
       </div>
     </div>
   )
