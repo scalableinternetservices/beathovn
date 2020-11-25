cd01ec02e3e902acfcd3ee7628b5359b6fadcc03
Sriram Balachandran <sriramb2000@gmail.com>
2020-11-25 08:52:29 -0800

    refactor: prettier

--- web/src/view/page/Post.tsx
+++ web/src/view/page/Post.tsx
@@ -1,7 +1,6 @@
-import CSS from 'csstype';
-import * as React from 'react';
-import { useState } from 'react';
-
+import CSS from 'csstype'
+import * as React from 'react'
+import { useState } from 'react'
 
 const containerStyle: CSS.Properties = {
   padding: '4px 32px',
@@ -17,35 +16,36 @@ const cardStyle: CSS.Properties = {
 const likeButtonStyle: CSS.Properties = {
   backgroundColor: '#000000',
   color: '#FFFFFF',
-  margin: '10 px'
+  margin: '10 px',
 }
 
 const commentButtonStyle: CSS.Properties = {
   backgroundColor: 'black',
   color: '#FFFFFF',
-  margin: '10px'
+  margin: '10px',
 }
 
 function likeButtonHandler() {
-  console.log("The like button was clicked!")
+  console.log('The like button was clicked!')
 }
 
-export function Post(props: {
-  commentary: string | null
-  musicLink: string | undefined
-}) {
-
+export function Post(props: { commentary: string | null; musicLink: string | undefined }) {
   const [displayComments, setDisplayComments] = useState(false)
+
   return (
     <div className="card" style={cardStyle}>
       <div className="container" style={containerStyle}>
-        <a href={props.musicLink} target="_blank">Song Link</a>
+        <a href={props.musicLink} target="_blank">
+          Song Link
+        </a>
         <h3>{props.commentary}</h3>
-        <button style={likeButtonStyle} type="button" onClick={likeButtonHandler}>Like</button>
-        <button style={commentButtonStyle} type="button" onClick={() => setDisplayComments(!displayComments)}>Show Comments</button>
-        {displayComments &&
-          <h3>WE ARE DISPLAYING THE COMMENTS</h3>
-        }
+        <button style={likeButtonStyle} type="button" onClick={likeButtonHandler}>
+          Like
+        </button>
+        <button style={commentButtonStyle} type="button" onClick={() => setDisplayComments(!displayComments)}>
+          Show Comments
+        </button>
+        {displayComments && <h3>WE ARE DISPLAYING THE COMMENTS</h3>}
       </div>
     </div>
   )
